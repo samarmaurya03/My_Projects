e425d735f292a865a50688e00922f72e1d5b88ec
Samar Maurya <samarmaurya1256@gmail.com>
2024-11-16 22:41:21 +0530

    second

--- clock/script.js
+++ clock/script.js
@@ -1,20 +1,20 @@
-let h = document.getElementById('h')
-let min = document.getElementById('m')
-let sec = document.getElementById('s')
+let h = document.getElementById('h');
+let m = document.getElementById('m');
+let s = document.getElementById('s');
 
 function displaytime() {
     let date = new Date();
     let hh = date.getHours();
     let mm = date.getMinutes();
-    let ss = date.getHours();
+    let ss = date.getSeconds();
 
-    let hrotate = 30*hh + mm/2;
-    let mrotate = 6*mm;
-    let srotate = 6*ss;
+    let hrotate = 30 * hh + mm / 2;
+    let mrotate = 6 * mm;          
+    let srotate = 6 * ss;          
 
-    h.style.transform=`rotate(${hrotate}deg)`;
-    h.style.transform=`rotate(${mrotate}deg)`;
-    h.style.transform=`rotate(${srotate}deg)`;
-
+    h.style.transform = `rotate(${hrotate}deg)`;
+    m.style.transform = `rotate(${mrotate}deg)`;
+    s.style.transform = `rotate(${srotate}deg)`;
 }
-setInterval(displaytime,1000)
+
+setInterval(displaytime, 1000); 
